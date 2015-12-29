6eef1c24f5685a787adc2289df470347f3edf007
ethush <ethush@gmail.com>
2015-12-28 18:07:47 -0600

    Done #23 Function

--- application/controllers/InvoiceController.php
+++ application/controllers/InvoiceController.php
@@ -14,6 +14,19 @@ class InvoiceController extends CI_Controller {
    * en caso de no encontrarse emite un mensaje de error.
    */
   public function get($id) {
+
+        $response["responseStatus"] = false;
+        if(is_numeric($id) && $id > 0) {
+          $this->load->model("InvoiceModel");
+          $invoice = $this->InvoiceModel->getInvoice($id);
+          echo json_encode($invoice);
+        }
+        else {
+          $response["responseStatus"] = "FAIL";
+          $response["message"] = "Registro no encontrado";
+          $this->output->set_content_type('application/json')->set_output(json_encode($response));
+        }
+
   }
 
 }
