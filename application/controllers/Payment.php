86f1fe462dad9ad1bb456685e58a5191ea1f2b46
Galileo Guzmán <galileoguzman@gmail.com>
2015-12-31 13:55:47 -0600

    Add payment controller

---
+++ application/controllers/Payment.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Payment extends CI_Controller {
+
+	
+}
+
+/* End of file payment.php */
+/* Location: ./application/controllers/payment.php */
