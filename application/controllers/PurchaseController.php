9d2e8dcc1ac08b429f49b918cbf6dbfafe73164d
Rick <rvcuag@hotmail.com>
2016-01-05 12:49:25 -0600

    Close #51 Purchase-Cancel-Request

--- application/controllers/PurchaseController.php
+++ application/controllers/PurchaseController.php
@@ -40,7 +40,31 @@ class PurchaseController extends CI_Controller {
      * pueda cancelar la compra completa.
      */
     public function cancelrequest() {
+      /*
+     * Funcion para solicitar una cancelación de compra, recibe el idPurchase
+     * activa a valor 1 el campo  solicitudCancelacion para que el administrador
+     * pueda cancelar la compra completa.
+     */
+    public function cancelrequest() {
+      
+        $idPurchase = $this->input->post("idPurchase");
+
+        $response["responseStatus"] = false;
+
+        $this->load->model("PurchaseModel");
+        $updated = $this->PurchaseModel->cancelRequest($idPurchase);
+
+        if($updated) {
+            $response["responseStatus"] = "Ok";
+            $response["message"] = "Solicitud de cancelacion exitosa";
+        }
+        else {
+            $response["responseStatus"] = "FAIL";
+            $response["message"] = "No se pudo procesar la solicitud";
+        }
+        $this->output->set_content_type('application/json')->set_output(json_encode($response));
     }
+    
 
     /*
      * Funcion para solicitar el listado de compra
